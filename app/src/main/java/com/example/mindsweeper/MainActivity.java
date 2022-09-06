b7c979632fb316c083a2fe1fe5aadff59bff938f
danial.asaria <dasaria@usc.edu>
2022-09-05 19:09:08 -0700

    changing the amount of blocks

--- app/src/main/java/com/example/mindsweeper/MainActivity.java
+++ app/src/main/java/com/example/mindsweeper/MainActivity.java
@@ -31,51 +31,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cell_tvs = new ArrayList<TextView>();
-
-
-        // Method (1): add statically created cells
-//        TextView tv00 = (TextView) findViewById(R.id.textView00);
-//        TextView tv01 = (TextView) findViewById(R.id.textView01);
-//        TextView tv10 = (TextView) findViewById(R.id.textView10);
-//        TextView tv11 = (TextView) findViewById(R.id.textView11);
-
-        //tv00 is the top left cell
-//        tv00.setTextColor(Color.GRAY);
-//        tv00.setBackgroundColor(Color.GRAY);
-//        tv00.setOnClickListener(this::onClickTV);
-
-//        tv01.setTextColor(Color.GRAY);
-//        tv01.setBackgroundColor(Color.GRAY);
-//        tv01.setOnClickListener(this::onClickTV);
-//
-//        tv10.setTextColor(Color.GRAY);
-//        tv10.setBackgroundColor(Color.GRAY);
-//        tv10.setOnClickListener(this::onClickTV);
-//
-//        tv11.setTextColor(Color.GRAY);
-//        tv11.setBackgroundColor(Color.GRAY);
-//        tv11.setOnClickListener(this::onClickTV);
-
-//        cell_tvs.add(tv00);
-//        cell_tvs.add(tv01);
-//        cell_tvs.add(tv10);
-//        cell_tvs.add(tv11);
+        GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
 
         // Method (2): add four dynamically created cells
 //        GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
-//        for (int i = 2; i<=3; i++) {
-//            for (int j=0; j<=1; j++) {
-//                TextView tv = new TextView(this);
-//                tv.setHeight( dpToPixel(64) );
-//                tv.setWidth( dpToPixel(64) );
-//                tv.setTextSize( 32 );//dpToPixel(32) );
-//                tv.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+        for (int i = 2; i<=5; i++) {
+            for (int j=0; j<=5; j++) {
+                TextView tv = new TextView(this);
+                tv.setHeight( dpToPixel(64) );
+                tv.setWidth( dpToPixel(64) );
+                tv.setTextSize( 32 );//dpToPixel(32) );
+                tv.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+                tv.setTextColor(Color.GRAY);
+                tv.setBackgroundColor(Color.GRAY);
+                tv.setOnClickListener(this::onClickTV);
+
+                GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
+                lp.setMargins(dpToPixel(2), dpToPixel(2), dpToPixel(2), dpToPixel(2));
+                lp.rowSpec = GridLayout.spec(i);
+                lp.columnSpec = GridLayout.spec(j);
+
+                grid.addView(tv, lp);
+                cell_tvs.add(tv);
+            }
+        }
+
+//         Method (3): add four dynamically created cells with LayoutInflater
+//        LayoutInflater li = LayoutInflater.from(this);
+//        for (int i = 1; i<=5; i++) {
+//            for (int j=0; j<=4; j++) {
+//                TextView tv = (TextView) li.inflate(R.layout.custom_cell_layout, grid, false);
+//                //tv.setText(String.valueOf(i)+String.valueOf(j));
 //                tv.setTextColor(Color.GRAY);
 //                tv.setBackgroundColor(Color.GRAY);
 //                tv.setOnClickListener(this::onClickTV);
 //
-//                GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
-//                lp.setMargins(dpToPixel(2), dpToPixel(2), dpToPixel(2), dpToPixel(2));
+//                GridLayout.LayoutParams lp = (GridLayout.LayoutParams) tv.getLayoutParams();
 //                lp.rowSpec = GridLayout.spec(i);
 //                lp.columnSpec = GridLayout.spec(j);
 //
@@ -84,27 +75,6 @@ public class MainActivity extends AppCompatActivity {
 //                cell_tvs.add(tv);
 //            }
 //        }
-
-        // Method (3): add four dynamically created cells with LayoutInflater
-        GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
-        LayoutInflater li = LayoutInflater.from(this);
-        for (int i = 1; i<=2; i++) {
-            for (int j=0; j<=1; j++) {
-                TextView tv = (TextView) li.inflate(R.layout.custom_cell_layout, grid, false);
-                //tv.setText(String.valueOf(i)+String.valueOf(j));
-                tv.setTextColor(Color.GRAY);
-                tv.setBackgroundColor(Color.GRAY);
-                tv.setOnClickListener(this::onClickTV);
-
-                GridLayout.LayoutParams lp = (GridLayout.LayoutParams) tv.getLayoutParams();
-                lp.rowSpec = GridLayout.spec(i);
-                lp.columnSpec = GridLayout.spec(j);
-
-                grid.addView(tv, lp);
-
-                cell_tvs.add(tv);
-            }
-        }
 
     }
 
